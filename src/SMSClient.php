a7440664b8876941bba01a5849e3c202fb1d4b2f
Back1ng <anatolyu128@gmail.com>
2023-11-24 17:10:27 +0500

    move from psr to curl

--- src/SMSClient.php
+++ src/SMSClient.php
@@ -3,53 +3,27 @@
 namespace Flagstudio\LaravelSmsDitrade;
 
 use Flagstudio\LaravelSmsDitrade\Contracts\SMSConfigurationInterface;
-use Flagstudio\LaravelSmsDitrade\Requests\GetSMSStatus;
 use Flagstudio\LaravelSmsDitrade\Requests\SendSMS;
-use Psr\Http\Client\ClientExceptionInterface;
-use Psr\Http\Client\ClientInterface;
-use Psr\Http\Message\RequestFactoryInterface;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\StreamFactoryInterface;
-use Psr\Http\Message\UriFactoryInterface;
 
 class SMSClient
 {
     public function __construct(
         private readonly SMSConfigurationInterface $configuration,
-        private readonly ClientInterface $client,
-        private readonly RequestFactoryInterface $requestFactory,
-        private readonly UriFactoryInterface $uriFactory,
-        private readonly StreamFactoryInterface $streamFactory,
     ) {}
 
-    /**
-     * @throws ClientExceptionInterface
-     */
-    public function send(SendSMS $request): ResponseInterface
+    public function send(SendSMS $request): bool|string
     {
-        $uri = $this->uriFactory->createUri(
-            $this->configuration->getHost() . $request->getAddress()
-        );
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $this->configuration->getHost() . $request->getAddress());
+        curl_setopt($curl, CURLOPT_POST, 1);
 
-        $data = $this->streamFactory->createStream(
-            json_encode($request->getRequest())
-        );
+        curl_setopt($curl, CURLOPT_POSTFIELDS, 'data=' . json_encode($request->getRequest()));
+        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+        curl_setopt($curl, CURLOPT_USERPWD, $this->configuration->getLogin() . ':' . $this->configuration->getPassword());
+        curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/x-www-form-urlencoded']);
+        curl_setopt($curl, CURLOPT_TIMEOUT_MS, 1000);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-        $request = $this->requestFactory
-            ->createRequest($request->getMethod(), $uri)
-            ->withHeader(
-                'Authorization',
-                'Basic ' . $this->configuration->getLogin() . ":" . $this->configuration->getPassword()
-            )
-            ->withHeader("Content-Type", "application/x-www-form-urlencoded")
-            ->withBody($data)
-        ;
-
-        return $this->client->sendRequest($request);
-    }
-
-    public function status(GetSMSStatus $request): ResponseInterface
-    {
-        // TODO
+        return curl_exec($curl);
     }
 }
